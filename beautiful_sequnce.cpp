d6f672c7d20dc751adb994ea3e5a341a4b3fe4ef
Rajan Chaurasia <chaurasiarajan3013@gmail.com>
2023-05-19 13:04:47 +0530

    first commit

---
+++ beautiful_sequnce.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        bool flag=false;
+        int a[n];
+        for(int i=0;i<n;i++){
+         cin>>a[i];
+         if(a[i]<=(i+1))
+           flag=true;
+           
+        }
+        if(flag)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
